1bcf6844c93d4730a8670db397a5030d37043e82
pandekar <pandeadhistanaya@gmail.com>
2023-03-19 10:35:10 +0800

    setup UITableView

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,10 +14,19 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "guten morgen!"),
+        Message(sender: "a@b.com", body: "morgen!"),
+        Message(sender: "1@2.com", body: "wie geht's?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -31,5 +40,37 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
+    }
+}
+
+//MARK: UITableViewDataSource
+/**
+ responsible to populating the tableView
+ */
+extension ChatViewController: UITableViewDataSource {
+
+    // return the number of rows required for cells display
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    //return the content data for each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        
+        return cell
+    }
+}
+
+//MARK: UITableViewDelegate
+/**
+ responsible for tableView interaction
+ */
+extension ChatViewController: UITableViewDelegate {
+
+    // interaction when tableView's cell is selected
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
 }
