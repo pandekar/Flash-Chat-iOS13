e9cf8d867a70594a603ed2dd116bae56de01c2fa
pandekar <pandeadhistanaya@gmail.com>
2023-03-20 16:26:55 +0800

    UI navigation bar improvements

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -13,6 +13,18 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: CLTypingLabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // hide navigation bar
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // show navigation bar after view will dissapear/move to another view
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.charInterval = 0.05
